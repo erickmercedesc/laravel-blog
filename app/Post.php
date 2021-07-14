fdb1af0f324bbe356ae8193a3fa3529850db9c8c
Erick Mercedes <erickmercedesemc@gmail.com>
2021-07-14 12:54:08 +0800

    Inicie Larevel Auth y Configure las migraciones, factory, seeder y modelo

---
+++ app/Post.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Cviebrock\EloquentSluggable\Sluggable;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use Sluggable;
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title',
+                'onUpdate' => true
+            ]
+        ];
+    }    
+    //
+    public function User()
+    {
+        return $this->belongsTo('App\User');
+    }
+}
