a92cb784d838519a29fead6618e6092c7c17ed22
Erick Mercedes <erickmercedesemc@gmail.com>
2021-07-16 00:22:14 +0800

    Agregue el contenido multimedia a la vista post

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -8,8 +8,15 @@
                     <div class="card-body">
                         <p class="text-muted">
                             {{$post->user->name}} &ndash; {{$post->created_at->format('d M Y')}}
-                        </p>                        
+                        </p>                       
                         <h4 class="card-title">{{$post->title}}</h4>
+
+                        @if($post->image)
+                            <img src="{{ $post->get_image }}" class="card-img-top">
+                        @elseif($post->iframe)
+                            <div class="embed-responsive embed-responsive-16by9">{{!! $post->iframe !!}}</div>
+                        @endif
+                                                
                         <p class="card-text">
                             {{$post->body}}
                         </p>
